febbd275c7e11f84a0c1d91742b17cd7bfe74db5
Tobias Koch <tok3@eq3w.de>
2020-05-07 09:44:38 +0200

    VERSION 1.2.2

--- lang/de/fe_lang.php
+++ lang/de/fe_lang.php
@@ -131,7 +131,7 @@ return [
     'quote'=>'Zitieren',
     'edit'=>'Bearbeiten',
     'search_topics'=>'Themen durchsuchen',
-
+    'search_topics'=>'Themen durchsuchen',
     ''=>'',
     ''=>'',
     '' => '',
